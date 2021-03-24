4e97e83c5090a529565c76a057cc0bc9d9cb7cdd
Lucas <lucas.67817@gmail.com>
2021-03-24 19:50:45 -0300

    Atualização de estilos

---
+++ src/pages/Home/style.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.div`
+    width: 80%;
+    margin: auto;
+
+    .categories{
+        display: flex;
+    }
+
+    .input-group{
+        position: relative;
+        top: -275px;
+        left:35%;         
+    }
+
+    .joker{
+        display: block;
+    }
+
+`
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    *{
+        padding: 0;
+        margin: 0;
+        
+    }
+    body{
+        background: #cccc;
+        font-family: 'Chango', cursive;
+    }
+
+    button{
+        width: 80px;
+        height: 35px;
+        background-color: white;
+        border-color: black;
+        margin-left: 10px;
+        color: black;
+        font-size: 13px;
+        font-weight: bold;
+    }
+    
+    input{
+        height: 31px;
+        width: 220px;
+        border-color: black;
+    }
+`
